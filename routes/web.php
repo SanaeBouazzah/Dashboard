fbf55b5a427227ae1abed247fb3b741309a5fb99
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-13 22:33:10 +0200

    b

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -26,3 +27,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::resource('categories', CategoryController::class);
